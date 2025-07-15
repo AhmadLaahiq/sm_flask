ee7ff1a0c996188871f801069cf962d5a8d4dfd5
Ibraaa <iibrazakiridwan@gmail.com>
2025-07-15 17:35:31 +0700

    app docker

--- app copy.py
+++ app copy.py
@@ -175,7 +175,5 @@ def prediksi():
         model_proba=None
     )
 
-import os
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    app.run(debug=True) 
